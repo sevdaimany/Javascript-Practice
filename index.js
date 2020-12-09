af6542ea9ff8c49fb38a0f130efbf270af9ef22e
sevda <imanysevda@gmail.com>
2020-12-09 14:24:13 +0330

    create timer class

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("hiiiiiiiiiii");
+class Timer{
+
+    constructor(durationInput, startButton, pauseButton){
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+
+        //set event listener
+
+        this.startButton.addEventListener('click', this.start);
+    }
+
+    //use arrow func for 'this' problem
+    start = () => {
+        console.log(this);
+    }
+
+    tick = () => {
+        console.log('tick');
+    }
+}
+
+
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector("#start");
+const pauseButton = document.querySelector("#pause");
+
+const timer = new Timer(durationInput , startButton , pauseButton);
