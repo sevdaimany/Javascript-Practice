fd5bd46fca49c9bf694e82804b8ddea162ccbbd2
sevda <imanysevda@gmail.com>
2020-12-11 14:15:16 +0330

    implemented a Reusable Debounce

--- movieFight/index.js
+++ movieFight/index.js
@@ -10,12 +10,21 @@ const fetchData = async (searchTerm) => {
 };
 const input = document.querySelector("input");
 
-const onInput = (event)=> {
-  if(timeoutId){
-    clearTimeout(timeoutId);
-  }
-   timeoutId = setTimeout( () => {
-        fetchData(event.target.value);
-    }, 1000);
-}
-input.addEventListener('input' , onInput);
+
+const debounce = (func, delay = 1000) => { 
+  let timeoutId;
+  return (...args) => {
+    if(timeoutId){
+      clearTimeout(timeoutId) 
+    }
+    timeoutId = setTimeout(() => {
+      func.apply(null , args);
+    } , delay)
+  };
+} 
+ 
+const onInput = (event) => {
+        fetchData(event.target.value,500);
+};
+
+input.addEventListener('input' ,debounce(onInput, 500));
