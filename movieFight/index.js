15d4b964f8344ef401b6b647bcf331c9e9df8b94
sevda <imanysevda@gmail.com>
2020-12-20 10:23:47 +0330

    opening a Menu

--- movieFight/index.js
+++ movieFight/index.js
@@ -11,7 +11,23 @@ const fetchData = async searchTerm => {
   }
   return response.data.Search;
 };
+
+const root = document.querySelector(".autocomplete");
+
+root.innerHTML = `
+<label><b>Search for a Movie</b></label>
+<input class = "input" />
+<div class = "dropdown">
+    <div class = "dropdown-menu">
+        <div class = "dropdown-content results" >< /div>
+    </div>
+</div>
+`; 
+
 const input = document.querySelector ('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector(".results" );
+ 
 
 const onInput = async event => {
   const movies = await fetchData (event.target.value);
