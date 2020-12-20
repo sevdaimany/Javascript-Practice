b2994dea9fe6db148fa600f454b7949ca3a15a46
sevda <imanysevda@gmail.com>
2020-12-20 08:50:18 +0330

    Rendering Movies

--- movieFight/index.js
+++ movieFight/index.js
@@ -5,7 +5,7 @@ const fetchData = async (searchTerm) => {
       s: searchTerm,
     }, 
   });
-
+  
   return response.data.Search;
 };
 const input = document.querySelector("input");
@@ -13,7 +13,16 @@ const input = document.querySelector("input");
  
 const onInput = async (event) => {
      const movies =  await fetchData(event.target.value);
-     console.log(movies);
+
+     for(let movie of movies){
+       const div = document.createElement("div");
+        div.innerHTML = `
+        <img src = "${movie.Poster}" />
+        <h1>${movie.Title}</h1>
+        `;
+
+        document.querySelector("#target").appendChild(div);
+     }
 };
 
 input.addEventListener('input' ,debounce(onInput, 500));
