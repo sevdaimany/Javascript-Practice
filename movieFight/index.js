f998de9f73cf5008dddfbbbb3ead124ff716e4e4
sevda <imanysevda@gmail.com>
2020-12-20 09:01:53 +0330

    handling errored responses

--- movieFight/index.js
+++ movieFight/index.js
@@ -1,28 +1,30 @@
-const fetchData = async (searchTerm) => {
-  const response = await axios.get('http://www.omdbapi.com/', {
+const fetchData = async searchTerm => {
+  const response = await axios.get ('http://www.omdbapi.com/', {
     params: {
       apikey: '4b54412e',
       s: searchTerm,
-    }, 
+    },
   });
-  
+
+  if (response.data.Error) {
+    return [];
+  }
   return response.data.Search;
 };
-const input = document.querySelector("input");
+const input = document.querySelector ('input');
 
- 
-const onInput = async (event) => {
-     const movies =  await fetchData(event.target.value);
+const onInput = async event => {
+  const movies = await fetchData (event.target.value);
 
-     for(let movie of movies){
-       const div = document.createElement("div");
-        div.innerHTML = `
+  for (let movie of movies) {
+    const div = document.createElement ('div');
+    div.innerHTML = `
         <img src = "${movie.Poster}" />
         <h1>${movie.Title}</h1>
         `;
 
-        document.querySelector("#target").appendChild(div);
-     }
+    document.querySelector ('#target').appendChild (div);
+  }
 };
 
-input.addEventListener('input' ,debounce(onInput, 500));
+input.addEventListener ('input', debounce (onInput, 500));
