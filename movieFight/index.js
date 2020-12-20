5a85586da6db12e955096affa2d072123f6b9f3b
sevda <imanysevda@gmail.com>
2020-12-20 12:44:44 +0330

    rendering an expanded summary

--- movieFight/index.js
+++ movieFight/index.js
@@ -8,7 +8,7 @@ const fetchData = async searchTerm => {
 
   if (response.data.Error) {
     return [];
-  } 
+  }
   return response.data.Search;
 };
 
@@ -29,14 +29,13 @@ const dropdown = document.querySelector ('.dropdown');
 const resultsWrapper = document.querySelector ('.results');
 
 const onInput = async event => {
-
   const movies = await fetchData (event.target.value);
 
-  if(!movies.length){
-    dropdown.classList.remove('is-active');
+  if (!movies.length) {
+    dropdown.classList.remove ('is-active');
     return;
   }
-   
+
   resultsWrapper.innerHTML = '';
   dropdown.classList.add ('is-active');
 
@@ -49,11 +48,11 @@ const onInput = async event => {
         <img src = "${imgSrc}" />
          ${movie.Title}
         `;
-    
-    option.addEventListener('click' , event => {
-      dropdown.classList.remove('is-active');
+
+    option.addEventListener ('click', event => {
+      dropdown.classList.remove ('is-active');
       input.value = movie.Title;
-      onMovieSelect(movie);
+      onMovieSelect (movie);
     });
 
     resultsWrapper.appendChild (option);
@@ -62,20 +61,37 @@ const onInput = async event => {
 
 input.addEventListener ('input', debounce (onInput, 500));
 
-document.addEventListener('click' , event => {
-
-  if(!root.contains(event.target)){
-    dropdown.classList.remove('is-active');
+document.addEventListener ('click', event => {
+  if (!root.contains (event.target)) {
+    dropdown.classList.remove ('is-active');
   }
-})
- 
+});
 
-const onMovieSelect = async (movie) => {
-  const response = await axios.get('http://www.omdbapi.com/', {
+const onMovieSelect = async movie => {
+  const response = await axios.get ('http://www.omdbapi.com/', {
     params: {
       apikey: '4b54412e',
       i: movie.imdbID,
-    }
-    });
-    console.log(response.data);
-  };
+    },
+  });
+
+  document.querySelector("#summary").innerHTML = movieTemplate(response.data);
+};
+
+const movieTemplate = movieDetail => {
+  return `
+      <article class = "media">
+        <figure class = "media-left">
+          <p class ="image">
+            <img src ="${movieDetail.Poster}"/>
+        </figure>
+        <div class= "media-content">
+          <div class ="content">
+              <h1>${movieDetail.Title}</h1>
+              <h4>${movieDetail.Genre}</h4>
+              <p>${movieDetail.Plot}</p>
+          </div>
+        </div>
+      </article>
+      `;
+};
