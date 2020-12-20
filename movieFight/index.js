bf3aadb144589f2c8749f7aa6449ad3cff2628fc
sevda <imanysevda@gmail.com>
2020-12-20 11:37:33 +0330

    Automatically Closing the Dropdown

--- movieFight/index.js
+++ movieFight/index.js
@@ -49,3 +49,10 @@ const onInput = async event => {
 };
 
 input.addEventListener ('input', debounce (onInput, 500));
+
+document.addEventListener('click' , event => {
+
+  if(!root.contains(event.target)){
+    dropdown.classList.remove('is-active');
+  }
+})
