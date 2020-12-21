f0ac2611d1734fea43de5587d35e47c3f05478e4
sevda <imanysevda@gmail.com>
2020-12-21 12:24:26 +0330

    hide the tutorial

--- movieFight/index.js
+++ movieFight/index.js
@@ -8,6 +8,7 @@ const autocompleteConfig = {
   },
 
   onOptionSelect (movie) {
+    document.querySelector(".tutorial").classList.add('is-hidden'); 
     onMovieSelect (movie);
   },
   inputValue (movie) {
