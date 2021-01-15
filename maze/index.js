71116c34e7d11d89fd115860b0b1c59c29476d02
sevda <imanysevda@gmail.com>
2021-01-15 18:04:07 +0330

    get content to appear

--- maze/index.js
+++ maze/index.js
@@ -1,1 +1,16 @@
-console.log('hi <3')
+const {Engine, Render, Runner , World, Bodies} = Matter;
+
+const engine = Engine.create();
+const {world} = engine;
+
+const render = Render.create({
+    element : document.body,
+    engine : engine,
+    option : {
+        width : 800 ,
+        height : 400,
+    }
+});
+
+Render.run(render);
+Runner.run(Runner.create() , engine);
