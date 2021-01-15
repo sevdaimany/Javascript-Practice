e9a31f7d717d0ff271bf359de4d2f4067f8e4fff
sevda <imanysevda@gmail.com>
2021-01-16 00:44:45 +0330

    add shuffle function

--- maze/index.js
+++ maze/index.js
@@ -7,12 +7,13 @@ const cells = 3;
 const width = 600;
 const height = 600;
 
+
 const render = Render.create ({
   element: document.body,
   engine: engine,
   options: {
     wireframes: true,
-    width ,
+    width,
     height,
   },
 });
@@ -20,37 +21,62 @@ const render = Render.create ({
 Render.run (render);
 Runner.run (Runner.create (), engine);
 
-
 //walls
 
 const walls = [
-  Bodies.rectangle (width /2, 0, width, 40, {isStatic: true}),
-  Bodies.rectangle (0, height /2, 40, height, {isStatic: true}),
-  Bodies.rectangle (width, height/2, 40, height, {isStatic: true}),
-  Bodies.rectangle (width/2, height, width, 40, {isStatic: true}),
+  Bodies.rectangle (width / 2, 0, width, 40, {isStatic: true}),
+  Bodies.rectangle (0, height / 2, 40, height, {isStatic: true}),
+  Bodies.rectangle (width, height / 2, 40, height, {isStatic: true}),
+  Bodies.rectangle (width / 2, height, width, 40, {isStatic: true}),
 ];
-World.add(world, walls);
+World.add (world, walls);
 
+
+const shuffle = (arr) => {
+  let counter = arr.length;
+
+  while(counter > 0){
+
+    const index = Math.floor(Math.random() * counter);
+    counter --;
+    [arr[index] , arr[counter]] = [arr[counter] , arr[index]];
+  }
+
+  return arr;
+
+}
 
 //maze generation
 
-const grid = Array(cells).fill(null).map(() => Array(cells).fill(false));
+const grid = Array (cells).fill (null).map (() => Array (cells).fill (false));
 
-const verticals = Array(cells).fill(null).map(() => Array(cells -1).fill(false));
+const verticals = Array (cells)
+  .fill (null)
+  .map (() => Array (cells - 1).fill (false));
 
-const horizontals = Array(cells -1).fill(null).map(() => Array(cells).fill(false));
+const horizontals = Array (cells - 1)
+  .fill (null)
+  .map (() => Array (cells).fill (false));
 
-const startRow = Math.floor(Math.random * cells)
-const startColumn = Math.floor(Math.random * cells);
+const startRow = Math.floor (Math.random * cells);
+const startColumn = Math.floor (Math.random * cells);
 
-
-const iterateThroughCells = (row , column) => {
-
+const iterateThroughCells = (row, column) => {
   //if i visited the cell at [row, column] then return
+  if (grid[row][column]) return;
 
   //Mark this cell as being visited
+  grid[row][column] = true;
 
   //assemble randomly-ordered list of neighbors
+  const neighbors = shuffle([
+    [row - 1, column],
+    [row + 1, column],
+    [row, column - 1],
+    [row, column + 1],
+  ]);
+
+  console.log(neighbors);
 
   //for each neighbor ..
 
@@ -61,7 +87,6 @@ const iterateThroughCells = (row , column) => {
   //remove a wall from either horizontals or verticals
 
   //visit that next cell
+};
 
-}
-
-iterateThroughCells(startRow ,startColumn);
+iterateThroughCells (1, 1);
