8904df6fe5d77be286ada24ce4456734458c02db
sevda <imanysevda@gmail.com>
2021-01-15 18:22:24 +0330

    draw border

--- maze/index.js
+++ maze/index.js
@@ -1,16 +1,26 @@
-const {Engine, Render, Runner , World, Bodies} = Matter;
+const {Engine, Render, Runner, World, Bodies} = Matter;
 
-const engine = Engine.create();
+const engine = Engine.create ();
 const {world} = engine;
 
-const render = Render.create({
-    element : document.body,
-    engine : engine,
-    option : {
-        width : 800 ,
-        height : 400,
-    }
+const render = Render.create ({
+  element: document.body,
+  engine: engine,
+  option: {
+    width: 800,
+    height: 600,
+  },
 });
 
-Render.run(render);
-Runner.run(Runner.create() , engine);
+Render.run (render);
+Runner.run (Runner.create (), engine);
+
+//walls
+
+const walls = [
+  Bodies.rectangle (400, 0, 800, 40, {isStatic: true}),
+  Bodies.rectangle (0, 300, 40, 600, {isStatic: true}),
+  Bodies.rectangle (800, 300, 40, 600, {isStatic: true}),
+  Bodies.rectangle (400, 600, 800, 40, {isStatic: true}),
+];
+World.add (world, walls);
