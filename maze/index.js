f95e5a74aee659ff3e52e0e351f7387c002cdcb2
sevda <imanysevda@gmail.com>
2021-01-16 19:57:46 +0330

    disabled gravity

--- maze/index.js
+++ maze/index.js
@@ -1,11 +1,12 @@
 const {Engine, Render, Runner, World, Bodies, Body} = Matter;
 
-const cells = 3;
+const cells = 10;
 const width = 600;
 const height = 600;
 const unitLength = width / cells;
 
 const engine = Engine.create ();
+engine.world.gravity.y = 0;
 const {world} = engine;
 const render = Render.create ({
   element: document.body,
