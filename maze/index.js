c5b8da51c77177303c14a4032461143712f6fde3
sevda <imanysevda@gmail.com>
2021-01-16 20:42:39 +0330

    add win animation

--- maze/index.js
+++ maze/index.js
@@ -1,6 +1,6 @@
 const {Engine, Render, Runner, World, Bodies, Body , Events} = Matter;
 
-const cells = 3 ;
+const cells = 6 ;
 const width = 600;
 const height = 600;
 const unitLength = width / cells;
@@ -122,6 +122,7 @@ horizontals.forEach ((row, indexRow) => {
       unitLength,
       10,
       {
+        label : 'wall',
         isStatic: true,
       }
     );
@@ -139,6 +140,7 @@ verticals.forEach ((row, indexRow) => {
       10,
       unitLength,
       {
+        label : 'wall',
         isStatic: true,
       }
     );
@@ -199,7 +201,12 @@ Events.on(engine , 'collisionStart' , event => {
     const labels = ['ball' , 'goal'];
 
     if(labels.includes(collision.bodyA.label) && labels.includes(collision.bodyB.label)){
-      console.log('u w on');
+      world.gravity.y = 1;
+      world.bodies.forEach(body =>{
+        if(body.label === 'wall'){
+          Body.setStatic(body , false);
+        }
+      });
     }
 
   });
