4989bf7bb695f3bb2bf8fe65a4374018cf590845
sevda <imanysevda@gmail.com>
2021-01-16 20:56:56 +0330

    scratch the canvas

--- maze/index.js
+++ maze/index.js
@@ -1,8 +1,8 @@
 const {Engine, Render, Runner, World, Bodies, Body , Events} = Matter;
 
 const cells = 6 ;
-const width = 600;
-const height = 600;
+const width = window.innerWidth;
+const height = window.innerHeight;
 const unitLength = width / cells;
 
 const engine = Engine.create ();
